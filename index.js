0708994d4a7f0b75a39da3ac9d5ce439233896d0
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-11-01 19:38:46 -0400

    finished post users

--- index.js
+++ index.js
@@ -11,26 +11,56 @@ server.use(cors());
 server.get('/api/users', async (req, res) => {
   try {
     const response = await db.find();
-    res.status(200).json(response);
+    return res.status(200).json(response);
   } catch (e) {
-    res.status(500).json({ error: 'The users information could not be retrieved.' });
+    return res.status(500).json({ error: 'The users information could not be retrieved.' });
   }
 });
 
 server.post('/api/users', async (req, res) => {
-  // fire away
+  const { name, bio } = req.body;
+  let response;
+
+  if (name === undefined || bio === undefined) {
+    return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+  }
+
+  try {
+    response = await db.insert({ name, bio });
+  } catch (e) {
+    return res.status(500).json({ error: 'There was an error while saving the user to the database' });
+  }
+
+  try {
+    response = await db.findById(response.id);
+    return res.status(201).json(response);
+  } catch (e) {
+    return res.status(500).json({ error: 'There was an error while fetching the user from the database'});
+  }
 });
 
 server.get('/api/users/:id', async (req, res) => {
-  // fire away
+  try {
+    //
+  } catch (e) {
+    res.status(500).json({ error: '' });
+  }
 });
 
 server.put('/api/users/:id', async (req, res) => {
-  // fire away
+  try {
+    //
+  } catch (e) {
+    res.status(500).json({ error: '' });
+  }
 });
 
 server.delete('/api/users/:id', async (req, res) => {
-  // fire away
+  try {
+    //
+  } catch (e) {
+    res.status(500).json({ error: '' });
+  }
 });
 
 server.listen(5000, () => console.log('listening on port 5000'));
