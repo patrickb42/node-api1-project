14c603db45bee93b1ebb1b4978b470034a59d13f
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-11-01 18:10:11 -0400

    scafolded out the requests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// implement your API here
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/api/users', async (req, res) => {
+  // fire away
+});
+
+server.post('/api/users', async (req, res) => {
+  // fire away
+});
+
+server.get('/api/users/:id', async (req, res) => {
+  // fire away
+});
+
+server.put('/api/users/:id', async (req, res) => {
+  // fire away
+});
+
+server.delete('/api/users/:id', async (req, res) => {
+  // fire away
+});
+
+server.listen(5000, () => console.log('listening on port 5000'));
