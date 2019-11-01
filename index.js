204a243bf5083578ba396dd2d357b28e898b1106
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-11-01 19:22:16 -0400

    finished get users

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
 server.get('/api/users', async (req, res) => {
-  // fire away
+  try {
+    const response = await db.find();
+    res.status(200).json(response);
+  } catch (e) {
+    res.status(500).json({ error: 'The users information could not be retrieved.' });
+  }
 });
 
 server.post('/api/users', async (req, res) => {
